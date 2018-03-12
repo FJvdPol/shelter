35cd4d419dc7d4529068eb5b5e0edab83ca0cf66
FJvdPol <folkertjanvanderpol@gmail.com>
2018-03-13 00:35:19 +0100

    Updated error handling functions and added support for uploading images & posting data via curl JSON

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 'use strict'
 
+var fs = require('fs')
 var express = require('express')
 var bodyParser = require('body-parser')
 var db = require('../db')
 var helpers = require('./helpers')
+var multer = require('multer')
+
+
+var upload = multer({dest: 'db/image'})
 
 
 module.exports = express()
@@ -11,12 +16,13 @@ module.exports = express()
   .set('views', 'view')
   .use(express.static('static'))
   .use('/image', express.static('db/image'))
+  .use(bodyParser.json())
   .use(bodyParser.urlencoded({extended: true}))
   .get('/', allAnimals)
   .get('/add', renderForm)
-  .post('/', addAnimal)
+  .post('/',  upload.single('image'), addAnimal)
   .get('/:id', getAnimal)
-  // .put('/:id', set)
+  // .put('/:id', setAnimal)
   // .patch('/:id', change)
   .delete('/:id', removeAnimal)
   .use(notFound)
@@ -28,28 +34,47 @@ function renderForm(req, res){
 
 function allAnimals(req, res) {
   var result = {errors: [], data: db.all()}
-
   res.render('list.ejs', Object.assign({}, result, helpers))
 }
 
 function addAnimal(req, res, next){
-    var data = helpers.configureAddData(req.body.data)
+    var data
+     // Following code was based off: https://stackoverflow.com/questions/23271250/how-do-i-check-content-type-using-expressjs
+    if (req.is('application/json')) {
+        data = req.body
+    } else {
+        data = helpers.configureAddData(req.body.data)
+    }
+    // end source
     var newAnimal
     try {
         newAnimal = db.add(data)
-        console.log("Added new animal!")
+        if (req.file){
+            fs.rename(req.file.path, 'db/image/'+newAnimal.id+'.jpg')
+        }
     } catch (err) {
-        result = {errors: [{id: '422', title: 'Unprocessable entity'}], data: undefined}
-        return res.status(422).render('error.ejs', Object.assign({}, result, helpers))
+        // following code was based of JonahGold's shelter assignment via: https://github.com/theonejonahgold/shelter/blob/master/server/index.js
+        if (req.file) {
+            fs.unlink(req.file.path, function (fsErr) {
+                if (fsErr) {
+                    return showErrorPage(500, {title: 'Internal Server Error'}, res, fsErr)
+                } else {
+                    return showErrorPage(422, {title: 'Unprocessable entity'}, res, fsErr)
+                }
+            })
+        } else {
+            return showErrorPage(422, {title: 'Unprocessable entity'}, res, err)
+        }
+        // end source
     }
     res.redirect('/' + newAnimal.id)
 }
+
 
 function getAnimal(req, res){
     var id = req.params.id
     var requestedAnimal = checkAnimal(id, res)
     if (requestedAnimal.exists) {
-        requestedAnimal.data = db.get(id)
         res.format({
             json: () => res.json(requestedAnimal),
             html: () => res.render('detail.ejs', Object.assign({}, requestedAnimal, helpers))
@@ -67,31 +92,49 @@ function removeAnimal(req, res, next){
 }
 
 function checkAnimal(id, res){
-    var has,
-    statusCode
+    var has
     var result = {errors: [], data: undefined}
     try {
         has = db.has(id)
     } catch (err) {
-        result.errors.push({id: '400', title: 'Bad request'})
-        res.status(400).render('error.ejs', Object.assign({}, result , helpers))
+        result = showErrorPage(400, {title: 'Bad request'}, res, err)
     }
     if (has) {
         result.exists = true
+        result.data = db.get(id)
     } else {
         if (db.removed(id)){
-            statusCode = 410
-            result.errors.push({id: '410', title: 'Page was deleted'})
+            result = showErrorPage(410, {title: 'Page was deleted'}, res)
         }
         else {
-            statusCode = 404
-            result.errors.push({id: '404', title: 'Page not Found'})
+            result = showErrorPage(404, {title: 'Page not found'}, res)
         }
-        res.status(statusCode).render('error.ejs', Object.assign({}, result, helpers))
     }
     return result
 }
 
+function showErrorPage(code, myErr, res, err) {
+    var result = {
+        errors: [{
+            id: code.toString(),
+            title: myErr.title
+        }],
+        data: undefined
+    }
+    if (err){
+        result.errors.push(err)
+        console.log(code, err)
+    }  else {
+        err = undefined
+    }
+    res.format({
+        json: () => res.status(code).json(result),
+        html: () => res.status(code).render('error.ejs', Object.assign({}, result, helpers))
+    })
+
+    return result
+}
+
 function notFound(err, req, res, next){
-    console.log("Uncaught error: ", err)
+    showErrorPage(500, {title: 'Internal server error'}, res, err)
 }
